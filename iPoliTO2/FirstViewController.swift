3fc92d02a228f5dc77111751cf1fa0104801af9b
Carlo Rapisarda <rapisarda.carlo@outlook.com>
2016-07-30 14:38:13 +0200

    Initial Commit

---
+++ iPoliTO2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iPoliTO2
+//
+//  Created by Carlo on 30/07/2016.
+//  Copyright © 2016 crapisarda. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iPoliTO2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iPoliTO2
+//
+//  Created by Carlo on 30/07/2016.
+//  Copyright © 2016 crapisarda. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
